7a82d2ef5ec2a82792a41da66050b5b8dfee0b82
BERNIE WARREN <bernie.warren@dcmail.ca>
2024-03-14 19:57:04 -0400

    Adding nav-link to task-list that only shows when logged in.

--- Lab3/Scripts/isLoggedIn.js
+++ Lab3/Scripts/isLoggedIn.js
@@ -2,10 +2,18 @@
 
 (() =>{
     // check if the user is already logged in
-    if(sessionStorage.getItem("user"))
-    {
-    // redirect to the secure area
-    loadHeader('contact-list');
-    loadContent('contact-list', activeLinkCallBack('contact-list'))
+    if(sessionStorage.getItem("user")) {
+        // redirect to the secure area
+        loadHeader('contact-list');
+        loadContent('contact-list', activeLinkCallBack('contact-list'))
+
+        // show the task list link
+        var taskList = document.getElementById('taskList');
+        taskList.style.display = 'block';
+
+    } else {
+        // hide the task list link
+        var taskList = document.getElementById('taskList');
+        taskList.style.display = 'none';
     }
 })();
